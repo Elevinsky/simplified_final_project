023285ead8a856b8e5c83d9aa0a8bcfa024b63f0
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate numericality of trainers.tues_end_hour

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -5,6 +5,8 @@ class Trainer < ApplicationRecord
 
   # Validations
 
+  validates :tues_end_hour, :numericality => { :greater_than => 0, :less_than_or_equal_to => 12 }
+
   validates :tues_end_min, :presence => true
 
   validates :tues_start_am_pm, :presence => true
