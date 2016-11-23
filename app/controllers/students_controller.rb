c7d883f29a6649466093204496cfb3ef6c919ea2
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:42 +0000

    Only student user can modify student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,14 @@
 class StudentsController < ApplicationController
+  before_action :current_user_must_be_student_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_student_user
+    student = Student.find(params[:id])
+
+    unless current_user == student.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @students = Student.all
 
