cc5ae85000241310756a00a61bf12369a525ae32
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:33:40 +0000

    Generate students resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Student resource:
+  # CREATE
+  get "/students/new", :controller => "students", :action => "new"
+  post "/create_student", :controller => "students", :action => "create"
+
+  # READ
+  get "/students", :controller => "students", :action => "index"
+  get "/students/:id", :controller => "students", :action => "show"
+
+  # UPDATE
+  get "/students/:id/edit", :controller => "students", :action => "edit"
+  post "/update_student/:id", :controller => "students", :action => "update"
+
+  # DELETE
+  get "/delete_student/:id", :controller => "students", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
