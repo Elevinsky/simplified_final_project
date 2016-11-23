c862e69d164443315cdbcc647b91e2a0465e8278
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate presence of trainers.wed_end_am_pm

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -5,6 +5,8 @@ class Trainer < ApplicationRecord
 
   # Validations
 
+  validates :wed_end_am_pm, :presence => true
+
   validates :wed_end_hour, :presence => true
 
   validates :wed_end_hour, :numericality => { :greater_than => 0, :less_than_or_equal_to => 12 }
