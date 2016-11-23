fb3fec4e7bd139cf9565128488eebe02da782bbe
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate presence of lessons.lesson_time

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,6 +5,8 @@ class Lesson < ApplicationRecord
 
   # Validations
 
+  validates :lesson_time, :presence => true
+
   validates :student_id, :presence => true
 
   validates :trainer_id, :presence => true
