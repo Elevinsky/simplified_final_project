6fd0ef948d0d0928e320a59d43e67c961895fff4
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate presence of trainers.fri_start_am_pm

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -5,6 +5,8 @@ class Trainer < ApplicationRecord
 
   # Validations
 
+  validates :fri_start_am_pm, :presence => true
+
   validates :fri_start_hour, :presence => true
 
   validates :fri_start_hour, :numericality => { :greater_than => 0, :less_than_or_equal_to => 12 }
