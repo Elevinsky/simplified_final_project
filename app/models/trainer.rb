6feeb61c23bf706eb2c305f1ae83240f3445f73e
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate numericality of trainers.sun_start_hour

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -5,6 +5,8 @@ class Trainer < ApplicationRecord
 
   # Validations
 
+  validates :sun_start_hour, :numericality => { :greater_than => 0, :less_than_or_equal_to => 12 }
+
   validates :sun_start_min, :presence => true
 
   validates :thur_end_am_pm, :presence => true
