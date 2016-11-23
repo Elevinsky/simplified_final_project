6262f623bdcfb1b84c1cd8dc4fa0f901c5cd67c8
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:42 +0000

    Whitelist columns in CancellationNote for ActiveAdmin

--- app/admin/cancellation_note.rb
+++ app/admin/cancellation_note.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CancellationNote do
 
+ permit_params :lesson_id, :body
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
