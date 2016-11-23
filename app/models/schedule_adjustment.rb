ca75001d40ab6a485b3bcb48ee68dceee8189674
Raghu Betina <raghu@firstdraft.com>
2016-11-23 01:34:00 +0000

    Validate presence of schedule_adjustments.adjusted_end

--- app/models/schedule_adjustment.rb
+++ app/models/schedule_adjustment.rb
@@ -5,6 +5,8 @@ class ScheduleAdjustment < ApplicationRecord
 
   # Validations
 
+  validates :adjusted_end, :presence => true
+
   validates :adjusted_start, :presence => true
 
   validates :trainer_id, :presence => true
