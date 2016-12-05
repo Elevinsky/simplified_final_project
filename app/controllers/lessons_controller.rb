c922316046f46c16fda225a4d936b63a553641bc
Erica Levinsky <elevinsky@gmail.com>
2016-12-04 18:34:50 -0600

    allow perfecta to add lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,7 +12,7 @@ class LessonsController < ApplicationController
     @lesson_options = ['9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM']
     @weekday = ['Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
     @q = Lesson.ransack(params[:q])
-    @lessons = @q.result(:distinct => true).includes(:student, :trainer, :cancellation_note).page(params[:page]).per(10)
+    @lessons = @q.result(:distinct => true).includes(:student, :trainer, :cancellation_note)
 
     @week = params[:id]
     render("lessons/index.html.erb")
